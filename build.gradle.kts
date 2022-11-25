9babcd5a4f343a6e90a577351c4fdea044d8ebd3
t-kurihara <takuya.kurihara00@gmail.com>
2022-11-25 22:56:14 +0900

    add lint.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,45 @@
-import org.jetbrains.kotlin.commonizer.OptimisticNumberCommonizationEnabledKey.alias
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 
 plugins {
     kotlin("jvm") version libs.versions.kotlin
     kotlin("plugin.spring") version libs.versions.kotlin
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    val targetDir = listOf("app").flatMap {
+        listOf(
+            "$it/src/main/kotlin",
+            "$it/src/test/kotlin",
+        )
+    }
+    buildUponDefaultConfig = true
+    allRules = false
+    source = files(targetDir)
+    config = files("$rootDir/config/detekt/detekt.yml")
+    baseline = file("$rootDir/config/detekt/baseline.xml")
+    basePath = projectDir.path
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true) // observe findings in your browser with structure and code snippets
+        xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+        txt.required.set(true) // similar to the console output, contains issue signature to manually edit baseline files
+        sarif.required.set(true) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with Github Code Scanning
+        sarif {
+            required.set(true)
+            outputLocation.set(file("/tmp/report/detekt.sarif"))
+        }
+        md.required.set(true) // simple Markdown format
+    }
+}
+
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+    jvmTarget = "1.8"
 }
 
 allprojects {
@@ -24,11 +58,13 @@ subprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
+            jvmTarget = "1.8"
         }
     }
 
     tasks.withType<Test> {
         useJUnitPlatform()
     }
+
+
 }
