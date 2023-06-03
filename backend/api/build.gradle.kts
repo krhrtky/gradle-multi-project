56a7967e676e77eaa7ba11f9d56a931af2c1ad22
takuya.kurihara <takuya.kurihara00@gmail.com>
2023-06-03 22:48:27 +0900

    feat: implement graphql.

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -3,6 +3,7 @@ import org.springframework.boot.gradle.tasks.run.BootRun
 plugins {
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
+    id("com.netflix.dgs.codegen") version "5.11.1"
 }
 
 dependencies {
@@ -11,6 +12,8 @@ dependencies {
     implementation(libs.jackson.module.kotlin)
     implementation(libs.kotlin.reflect)
     implementation(libs.kotlin.stdlib)
+    implementation(platform(libs.graphql.dgs.platform))
+    implementation(libs.graphql.dgs.spring.boot)
     implementation(project(":backend:domains"))
     implementation(project(":backend:infrastructure"))
     testImplementation(libs.spring.starter.test) {
---
+++ backend/api/src/main/kotlin/com/example/applications/controllers/graphql/GraphQLController.kt
@@ -0,0 +1,32 @@
+package com.example.applications.controllers.graphql
+
+import com.example.domains.applications.users.UserApplicationService
+import com.example.domains.applications.users.UserCreateInput
+import com.netflix.dgs.codegen.generated.types.CreateUserInput
+import com.netflix.dgs.codegen.generated.types.User
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsMutation
+import com.netflix.graphql.dgs.DgsQuery
+import com.netflix.graphql.dgs.InputArgument
+
+@DgsComponent
+class GraphQLController(
+    private val service: UserApplicationService,
+) {
+    @DgsQuery
+    fun fetchUser(@InputArgument id: String): User? =
+        service
+                .find(id)
+                .getOrNull()
+                ?.map(::User)
+
+    @DgsMutation
+    fun createUser(@InputArgument input: CreateUserInput): User =
+        UserCreateInput(
+            name = input.name,
+            email = input.email,
+        )
+            .let(service::create)
+            .getOrThrow()
+            .map(::User)
+}
