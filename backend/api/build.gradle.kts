e5cde38ee9f48b516208f3dc37749658255fa197
t-kurihara <takuya.kurihara00@gmail.com>
2023-06-07 21:36:48 +0900

    feat: implement graphql. (#23)
    * feat: implement graphql.
    
    * feat: add make command.
    
    * feat: move schema file to project root.
    
    * fix: rename import package name.
    
    * refactor

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
+    id("com.netflix.dgs.codegen") version "5.11.1"
 }
 
 dependencies {
@@ -11,10 +13,17 @@ dependencies {
     implementation(libs.jackson.module.kotlin)
     implementation(libs.kotlin.reflect)
     implementation(libs.kotlin.stdlib)
+    implementation(platform(libs.graphql.dgs.platform))
+    implementation(libs.graphql.dgs.spring.boot)
     implementation(project(":backend:domains"))
     implementation(project(":backend:infrastructure"))
     testImplementation(libs.spring.starter.test) {
         exclude("org.junit.vintage:junit-vintage-engine")
     }
     testImplementation(kotlin("test"))
+}
+
+tasks.generateJava {
+    schemaPaths = mutableListOf("${rootProject.projectDir}/schema.graphql")
+    packageName = "com.example.applications.graphql"
 }
