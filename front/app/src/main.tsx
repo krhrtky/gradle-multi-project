428b9071c087b65b5be8f69cf7bc0da44a5bb813
takuya.kurihara <takuya.kurihara00@gmail.com>
2023-11-03 13:43:50 +0900

    feat: update Axios baseURL to use environment variable

--- front/app/src/main.tsx
+++ front/app/src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import Axios from "axios";
+import { env } from "./libs/env";
 
-Axios.defaults.baseURL = "http://localhost:5173/api";
+Axios.defaults.baseURL = env.BACKEND_BASE_URL
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
